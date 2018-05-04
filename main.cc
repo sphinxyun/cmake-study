23b68fbf1487d0ebbfb3688dcf34131f5115823f
sphinxyun <sphinx_yun@163.com>
2018-05-04 14:22:44 +0000

    cmake example1

---
+++ main.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+double power(double base,int exponent)
+{
+	double result= base;
+	int i;
+	if(exponent==0)
+		result=1;
+	for(i=1;i<exponent;i++)
+		result=result*base;
+	return result;
+}
+
+int main(int argc, char *argv[])
+{	
+	if(argc <3){
+	printf("usage: %s base exponent \n",argv[0]);
+	return 1;
+	}
+	double base = atof(argv[1]);
+	int exponent = atoi (argv[2]);
+	double result = power(base,exponent);
+	printf("%g ^ %d is %g\n",base,exponent,result);
+	return 0;
+}
